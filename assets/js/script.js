781c60ba1890197c9870278e0599abf4a25586ba
Monoget <monoget2@gmail.com>
2022-01-29 16:51:03 +0600

    update files

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,30 @@ $(".progress-bar-striped").each(function(i){
 		}
 	});
 });
+
+let members = $(".team__members");
+let isHover = false;
+function mediaSize() {
+
+	$(members).hover(
+
+		() => {
+			if (window.matchMedia('(min-width: 480px)').matches) {
+				$(members).removeClass("team__members--show");
+				isHover = true;
+				console.log('hover');
+			}
+		},
+		() => {
+			if (window.matchMedia('(min-width: 480px)').matches) {
+				isHover = false;
+			}
+		}
+	);
+
+}
+
+/* Call the function */
+mediaSize();
+/* Attach the function to the resize event listener */
+window.addEventListener('resize', mediaSize, false);
